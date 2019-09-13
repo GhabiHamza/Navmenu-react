1bf383c99a9f99f1bcf89174f530973f9b9b75d3
GhabiHamza <ghabihamza77@gmail.com>
2019-09-13 14:30:28 +0200

    Updates

---
+++ precache-manifest.a4438c8470e235485154b45dd4116ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4ec78d8669c9a934c46c044e8aba55",
+    "url": "/Navmenu-react/index.html"
+  },
+  {
+    "revision": "bca121dc00b161ef9194",
+    "url": "/Navmenu-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "60cdf83c6d19bfa76a90",
+    "url": "/Navmenu-react/static/css/main.4e41e13b.chunk.css"
+  },
+  {
+    "revision": "bca121dc00b161ef9194",
+    "url": "/Navmenu-react/static/js/2.789d7ecc.chunk.js"
+  },
+  {
+    "revision": "60cdf83c6d19bfa76a90",
+    "url": "/Navmenu-react/static/js/main.b9948124.chunk.js"
+  },
+  {
+    "revision": "de76abb7e6f8b747c24a",
+    "url": "/Navmenu-react/static/js/runtime~main.dc49bbd0.js"
+  }
+]);
